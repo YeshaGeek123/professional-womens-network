2511879f3d39f28227e33bd470a7b8bfe14a5d62
YeshaGeek123 <yeshageek435@gmail.com>
2022-06-23 07:59:34 +0530

    23-6-22

--- template-parts/content-scholarship.php
+++ template-parts/content-scholarship.php
@@ -8,15 +8,15 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="about-us-text">
-                    <h1 class="h2-title">The PWN Scholarship Foundation</h1>
+                    <h1 class="h2-title"><?php the_field('scholarship_title'); ?></h1>
                     <div class="overflow-text" data-simplebar="init">
-                       <p>Established to financially assist non-traditional women students, the Professional Women’s Network Scholarship Foundation also provides recipients with encouragement and emotional support from the organization’s members.</p>
+                        <?php the_field('scholarship_content'); ?>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="about-img">
-                    <div class="back-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/06/gallery-img5.jpg');"></div>
+                    <div class="back-img" style="background-image: url('<?php the_field('scholarship_image'); ?>');"></div>
                 </div>
             </div>
         </div>
@@ -30,13 +30,13 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="scholarship-img">
-                    <div class="back-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/06/ilus-scolarshio-use.svg');"></div>
+                    <div class="back-img" style="background-image: url('<?php the_field('scholarship_uses_image'); ?>');"></div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="scholarship-content">
-                    <h2 class="h2-title">Scholarship Use</h2>
-                    <p>Scholarships may be used by the recipients in any manner. Past recipients have used the funds to pay tuition, purchase books, cover the cost of childcare or transportation, among other things.</p>
+                    <h2 class="h2-title"><?php the_field('scholarship_uses_title'); ?></h2>
+                    <?php the_field('scholarship_uses_content'); ?>
                 </div>
             </div>
         </div>
@@ -51,10 +51,19 @@
         <div class="row">
             <div class="col-lg-7">
                 <div class="requirements-content">
-                    <h2 class="h2-title">Requirements</h2>
-                    <p>Women returning to college or seeking a post-secondary degree must meet the following requirements:</p>
+                    <h2 class="h2-title"><?php the_field('requirements_title'); ?></h2>
+                   <?php the_field('requirements_content'); ?>
                     <div class="requirements-text">
-                       <p>Must be a female resident of Iowa.</p>
+                        <?php
+                        if(have_rows('requirements_lists')):
+                            while(have_rows('requirements_lists')): the_row();
+                        ?>
+                            <p><?php the_sub_field('requirements_points'); ?></p>
+                        <?php
+                            endwhile;
+                        endif;
+                        ?>
+                       <!-- <p>Must be a female resident of Iowa.</p>
                        <p>Must be a student who will be attending classes, either full or part-time, during the academic year.</p>
                        <p>Must be working towards a vocational certification, associate degree, or first bachelor’s degree at one of the educational institutions listed below.</p>
                        <ul>
@@ -71,7 +80,7 @@
                            <li>St. Luke’s Medical Technology Program</li>
                            <li>A collegiate institution not listed above but located within a 60-mile radius of Linn County, Iowa.</li>
                        </ul>
-                       <p>Special consideration will be given to non-traditional students, defined as those at least 21 years old upon application deadline who have or will return to school after an absence of at least 3 years.</p>
+                       <p>Special consideration will be given to non-traditional students, defined as those at least 21 years old upon application deadline who have or will return to school after an absence of at least 3 years.</p> -->
                     </div>
                 </div>
             </div>
